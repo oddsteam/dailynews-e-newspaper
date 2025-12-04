6d2074b653a055e9813106b1b3fe88c56429e85c
Pongsaphak <39190909+pongsaphakmw@users.noreply.github.com>
2025-12-04 17:13:04 +0700

    add omise helper to mock api key for test env

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -22,4 +22,13 @@ module ApplicationHelper
 
     current_user.subscriptions.any?(&:active?)
   end
+
+  # Use test public key in test environment
+  def omise_public_key
+    if Rails.env.test?
+      "pkey_test_no_network"
+    else
+      Rails.application.credentials.dig(:omise, :public_key)
+    end
+  end
 end
