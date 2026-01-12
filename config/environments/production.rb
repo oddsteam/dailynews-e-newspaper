64023e1893689a46aa774b074d280a088ba81c1e
synce.2546 <synce.2546@gmail.com>
2026-01-12 10:39:20 +0700

    fix: the content missing bug in sign in and sign up modal

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,7 @@ Rails.application.configure do
   config.active_storage.service = :huaweicloud
 
   # Set the relative URL root for apps behind load balancer with path prefix
-  config.relative_url_root = "/e-newspaper"
+  config.relative_url_root = Rails.application.credentials.dig(:turbo, :root) || "/e-newspaper"
 
   # config.assets.prefix = "/e-newspaper/assets"
 
